26d79160b6826d33ca3ef494d591dfd4c80b1d30
soumayasassi <soumaya.sassimahfoudh@esprit.tn>
2024-02-15 17:02:25 +0100

    correction atelier

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,19 +2,48 @@ import { Button } from "react-bootstrap";
 import viteLogo from "/vite.svg";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import { useState } from "react";
 function Event(props) {
-    
+  const [event, setEvent] = useState(props.event);
+  const handleBuy = () => {
+    props.showAlert();
+    setEvent((prev) => ({
+      ...prev,
+      nbParticipants: prev.nbParticipants + 1,
+      nbTickets: prev.nbTickets - 1,
+    }));
+  };
+  const handleLike = () => {
+    setEvent((prev) => ({
+      ...prev,
+      like: !prev.like,
+    }));
+  };
   return (
-    <Col style={{ width: "20%" , margin : "10px" }} className="m-2">
+    <Col style={{ width: "20%", margin: "10px" }} className="m-2">
       <Card>
-        <Card.Img variant="top" src={"../images/" + props.event.img} />
+        <Card.Img
+          variant="top"
+          src={`/images/${event.nbTickets ? event.img : "sold_out.png"}`}
+        />
+
         <Card.Body>
-          <Card.Title>{props.event.name}</Card.Title>
-          <Card.Text>Price : {props.event.price}</Card.Text>
-          <Card.Text>Number of tickets : {props.event.nbTickets}</Card.Text>
-          <Card.Text>
-            Number of participants : {props.event.nbParticipants}
-          </Card.Text>
+          <Card.Title>{event.name}</Card.Title>
+          <Card.Text>Price : {event.price}</Card.Text>
+          <Card.Text>Number of tickets : {event.nbTickets}</Card.Text>
+          <Card.Text>Number of participants : {event.nbParticipants}</Card.Text>
+          <Button onClick={handleLike}>
+            {" "}
+            {event.like ? "Dislike" : "Like"}
+          </Button>
+          <Button
+            variant="primary"
+            onClick={handleBuy}
+            disabled={event.nbTickets ? false : true}
+          >
+            {" "}
+            Book Event
+          </Button>
         </Card.Body>
       </Card>
     </Col>
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,20 +1,35 @@
 import events from "../events.json";
-import { Container, Row , Alert } from "react-bootstrap";
+import { Container, Row, Alert } from "react-bootstrap";
 import Event from "./Event";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Events() {
- 
+  const [showalert, setShowalert] = useState(false);
+  const [welcome, setWelcome] = useState(true);
+  const showAlert = () => {
+    setShowalert(true);
+    setTimeout(() => {
+      setShowalert(false);
+    }, 5000);
+  };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setWelcome(false)
+    }, 3000);
+  }, []);
   return (
+    <>
+    { welcome && (<Alert> Hey Welcome to Esprit Events</Alert>)}
     <Container style={{ marginTop: "30px" }}>
-      
       <Row xs={1} md={4} className="g-4">
         {events.map((eventItem, index) => (
-          <Event key={index} event={eventItem}  />
+          <Event key={index} event={eventItem} showAlert={showAlert} />
         ))}
       </Row>
+      {showalert && <Alert> You have book an event </Alert>}
     </Container>
+    </>
   );
 }
 
